8d12cc6d216210a8d743e085599b794ccb8bc95e
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-09-15 23:55:24 -0700

    Seed database with flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,17 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 Airport.delete_all
+Flight.delete_all
 
-['ATL', 'DFW', 'DEN', 'LAX', 'LAS', 'PHX', 'MIA', 'SEA', 'SFO'].each do |airport_code|
+%w[ATL DFW DEN LAX LAS PHX MIA SEA SFO].each do |airport_code|
   Airport.create(airport_code: airport_code)
   puts "Airport Created: #{airport_code}"
+end
+
+10.times do
+  airport_ids = [*1..9].sample(2)
+  departure_date = Time.zone.now - rand(1..4.years) - rand(1..31).days
+  Flight.create(departure_airport_id: airport_ids[0], arrival_airport_id: airport_ids[1],
+                departure_date: departure_date, flight_duration: rand(1..12))
+  puts "Flight Created: From #{Flight.last.departure_airport.airport_code} to #{Flight.last.arrival_airport.airport_code}"
 end
